a80e16cfdf559fd32c8d04e1fa9e9a49d8fd4915
suryakatari97 <48117743+suryakatari97@users.noreply.github.com>
2019-05-06 19:52:21 -0700

    Update imageProcessing.py

--- src/imageProcessing.py
+++ src/imageProcessing.py
@@ -2,20 +2,21 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 img = cv.imread('test3.jpg',0)
-# global thresholding
-ret1,th1 = cv.threshold(img,0,255,cv.THRESH_BINARY)
-# Otsu's thresholding
-ret2,th2 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-# Otsu's thresholding after Gaussian filtering
-blur = cv.GaussianBlur(img,(5,5),0)
-ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-# plot all the images and their histograms
-#cv.imshow('img',th3)
-fnoise=cv.medianBlur(th3,3)
-cv.imshow('img',fnoise)
+block_size = 9
+constant = 2
+k = np.ones((5,5),np.uint8)
+blur = cv.GaussianBlur(img,(7,7),0)
+fnoise=cv.medianBlur(blur,3)
+th1 = cv.adaptiveThreshold(fnoise, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, block_size, constant)
+th2 = cv.adaptiveThreshold (fnoise, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, block_size, constant)
+#blu = cv.GaussianBlur(th2,(9,9),0)
+fnois=cv.medianBlur(th2,3)
+#erosion=cv.erode(fnois, k, iterations=1)
+dilation=cv.dilate(fnois, k, iterations=5)
+cv.imshow('img',dilation)
 cv.waitKey(0)
 cv.destroyAllWindows()  
-cv.imwrite('t3.png',th3) 
-plt.imshow(th3)
+cv.imwrite('t56.png',fnois) 
+plt.imshow(th2)
 plt.title('image') 
-plt.show()
+plt.show() 
